9a72a974628b28d80dffa330d4d7cde37e384b64
Debjyoti777 <djdebjyoti777@gmail.com>
2026-08-20 20:04:28 +0530

    Use environment database URL

--- database.py
+++ database.py
@@ -1,30 +1,39 @@
 import os
+from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.engine import URL
 
 
-DATABASE_URL = URL.create(
-    drivername="postgresql+psycopg",
-    username="postgres",
-    password="djtherock@99",
-    host=os.getenv("DB_HOST", "localhost"),    port=5432,
-    database="devconnect_db"
-)
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    database_url = DATABASE_URL
+else:
+    database_url = URL.create(
+        drivername="postgresql+psycopg",
+        username="postgres",
+        password="djtherock@99",
+        host="localhost",
+        port=5432,
+        database="devconnect_db"
+    )
+
+
+engine = create_engine(database_url)
 
 Base = declarative_base()
-
-with engine.connect() as connection:
-    print("Database connected successfully!")
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
 
 def get_db():
     db = SessionLocal()
